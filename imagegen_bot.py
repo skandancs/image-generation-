fea14503b41d1c9482297d07ea20d44ab99fbd3e
skandancs <skandancs@gmail.com>
2026-01-22 18:45:30 +0530

    Update imagegen_bot.py

--- imagegen_bot.py
+++ imagegen_bot.py
@@ -1,26 +1,27 @@
 import streamlit as st
-from groq import Groq
+from huggingface_hub import InferenceClient
+from PIL import Image
+import io
 
 st.set_page_config("Bodha AI by Skandan", layout="wide")
 st.title("Bodha AI")
 
-client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+client = InferenceClient(
+    "Tongyi-MAI/Z-Image-Turbo",
+    token=st.secrets["HF_TOKEN"]
+)
+
 col1, col2 = st.columns(2)
 
 st.image("bodhaai.jpeg", width=150, caption="Bodha AI")
 
-
 with col1:
-    product = st.text_input("Product")
-    audience = st.text_input("Audience")
+    inputprompt = st.text_input("Enter the Description")
 
     if st.button("Generate Content"):
-        prompt = f"Write marketing content for {product} targeting {audience}."
-        response = client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
-            messages=[{"role": "user", "content": prompt}]
-        )
-        st.session_state.text = response.choices[0].message.content
+        with st.spinner("Generating image..."):
+            image = client.text_to_image(inputprompt)
+            st.session_state.image = image
 
 with col2:
     if "image" in st.session_state:
@@ -37,5 +38,3 @@ with col2:
         )
     else:
         st.info("Generate content first")
-
-

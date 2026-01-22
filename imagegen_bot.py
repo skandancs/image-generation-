aa916db034a1a183981d86a9af093fd065c048cc
skandancs <skandancs@gmail.com>
2026-01-22 13:40:34 +0530

    Update imagegen_bot.py

--- imagegen_bot.py
+++ imagegen_bot.py
@@ -1,4 +1,4 @@
-
+'''
 import streamlit as st
 import torch
 from diffusers import StableDiffusionPipeline
@@ -42,10 +42,7 @@ with col2:
     else:
         st.info("Generate content first")
 
-
-
-
-'''
+----------------------------------------------------------------
 import streamlit as st
 from huggingface_hub import InferenceClient
 from PIL import Image
@@ -87,3 +84,44 @@ with col2:
     else:
         st.info("Generate content first")
 '''
+
+import streamlit as st
+from huggingface_hub import InferenceClient
+from PIL import Image
+import io
+
+st.set_page_config("Bodha AI by Skandan", layout="wide")
+st.title("Bodha AI")
+
+client = InferenceClient(
+    "runwayml/stable-diffusion-v1-5",
+    token=st.secrets["HF_TOKEN"]
+)
+
+col1, col2 = st.columns(2)
+
+st.image("bodhaai.jpeg", width=150, caption="Bodha AI")
+
+with col1:
+    inputprompt = st.text_input("Enter the Description")
+
+    if st.button("Generate Content"):
+        with st.spinner("Generating image..."):
+            image = client.text_to_image(inputprompt)
+            st.session_state.image = image
+
+with col2:
+    if "image" in st.session_state:
+        st.image(st.session_state.image, width=400)
+
+        img_buffer = io.BytesIO()
+        st.session_state.image.save(img_buffer, format="PNG")
+
+        st.download_button(
+            label="⬇️ Download Image",
+            data=img_buffer.getvalue(),
+            file_name="generated.png",
+            mime="image/png"
+        )
+    else:
+        st.info("Generate content first")
